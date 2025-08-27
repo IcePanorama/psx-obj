988df3f28b5017d4997f44d89f4ef7ce48d0a448
IcePanorama <thriftedroot@protonmail.com>
2025-08-27 14:25:31 -0400

    Refactored code.

--- src/Q3_12/Q3_12.cs
+++ src/Q3_12/Q3_12.cs
@@ -1,21 +1,22 @@
-using System.Diagnostics;
-
 /// A signed, fixed point (Q3.12) decimal numeral. Has a valid range of -8 to
-/// 7.999755859375. Not doing any rounding.
+/// 7.999755859375. Not doing any rounding, just cause.
 class Q3_12
 {
     static readonly int FRACTIONAL_BITS = 12;
+    static readonly int ONE = 1 << FRACTIONAL_BITS;
     static readonly float MIN_SUPPORTED_VALUE = -8.0f;
     static readonly float MAX_SUPPORTED_VALUE = 7.999755859375f;
 
     public short value { get; private set; } = 0;
 
-    Q3_12(float f)
+    public Q3_12(float f)
     {
-        Debug.Assert((MIN_SUPPORTED_VALUE < f)
-                     || Math.Abs(MIN_SUPPORTED_VALUE - f) < double.Epsilon);
-        Debug.Assert((f < MAX_SUPPORTED_VALUE)
-                     || Math.Abs(f - MAX_SUPPORTED_VALUE) < double.Epsilon);
-        value = (short)(f * (1 << FRACTIONAL_BITS));
+        if ((f < MIN_SUPPORTED_VALUE) || (MAX_SUPPORTED_VALUE < f))
+            throw new
+                ApplicationException(
+                    "float is outside of the range of a Q3.12 "
+                    + string.Format("fixed-point number: {0}", f));
+        value = (short)(f * ONE);
+        Console.WriteLine(string.Format("Before: {0}, after: {1}", f, value));
     }
 }
