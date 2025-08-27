cf74a09ac50f1d5340546111c983c9509ddd9e3e
IcePanorama <thriftedroot@protonmail.com>
2025-08-27 14:30:45 -0400

    Using Q3.12s in vertex now.

--- src/WavefrontObj/Vertex.cs
+++ src/WavefrontObj/Vertex.cs
@@ -2,9 +2,17 @@ namespace WavefrontObj
 {
     class Vertex
     {
+        public Q3_12[] coords { get; private set; } = new Q3_12[3];
+
+        public Q3_12 x => coords[0];
+        public Q3_12 y => coords[1];
+        public Q3_12 z => coords[2];
+
         public Vertex(string x, string y, string z)
         {
-            Console.WriteLine(x + " " + y + " " + z);
+            coords[0] = new Q3_12(float.Parse(x));
+            coords[0] = new Q3_12(float.Parse(y));
+            coords[0] = new Q3_12(float.Parse(z));
         }
     }
 }
