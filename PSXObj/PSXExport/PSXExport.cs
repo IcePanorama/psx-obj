438621153f0e1a4d65608c20aaeaf97f14c39c3a
IcePanorama <thriftedroot@protonmail.com>
2025-09-05 15:01:56 -0400

    Refactored code.

--- PSXObj/PSXExport/PSXExport.cs
+++ PSXObj/PSXExport/PSXExport.cs
@@ -12,21 +12,21 @@ namespace PSXExport
 
         protected PSXExport(WavefrontObjFile w)
         {
+            /// Creates a MS-DOS/8.3 compliant filename from the given
+            /// filename. This base class does not add an extension, leaving
+            /// that for subclasses to implement.
+            string CreateFilename(string filename)
+            {
+                int ext = filename.LastIndexOf('.');
+                ext = ext != -1 ? Math.Min(ext, 9) : filename.Length;
+                return filename.Substring(0, ext).ToUpper();
+            }
+
             Console.WriteLine("Before: " + w.filePath);
             _filename = CreateFilename(w.filePath);
             Console.WriteLine("After: " + _filename);
             _verts = w.verts;
             _tris = w.tris;
-        }
-
-        /// Creates a MS-DOS/8.3 compliant filename from the given filename.
-        /// This base class does not add an extension, leaving that up for
-        /// subclasses to implement.
-        string CreateFilename(string filename)
-        {
-            int ext = filename.LastIndexOf('.');
-            ext = ext != -1 ? Math.Min(ext, 9) : filename.Length;
-            return filename.Substring(0, ext).ToUpper();
         }
     }
 }
