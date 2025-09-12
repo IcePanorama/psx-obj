fe4f50625bc9f5d62105086933616653850ac49f
IcePanorama <thriftedroot@protonmail.com>
2025-09-12 12:50:54 -0400

    refactoring.

--- PSXObj/WavefrontObj/GenericVec3.cs
+++ PSXObj/WavefrontObj/GenericVec3.cs
@@ -3,14 +3,5 @@ namespace WavefrontObj
         public abstract class GenericVec3<T>
         {
                 protected T[] data = new T[3];
-
-                /*
-                public GenericVec3(T v0, T v1, T v2)
-                {
-                        data[0] = v0;
-                        data[1] = v1;
-                        data[2] = v2;
-                }
-                */
         }
 }
--- PSXObj/WavefrontObj/Vertex.cs
+++ PSXObj/WavefrontObj/Vertex.cs
@@ -1,35 +1,17 @@
 namespace WavefrontObj
 {
-    public class Vertex
+    public class Vertex : GenericVec3<float>
     {
-        /*
-        public Q3_12[] coords { get; private set; } = new Q3_12[3];
-        public Q3_12 x => coords[0];
-        public Q3_12 y => coords[1];
-        public Q3_12 z => coords[2];
-        */
-        public float[] coords { get; private set; } = new float[3];
+        public float[] coords => data;
         public float x => coords[0];
         public float y => coords[1];
         public float z => coords[2];
 
         public Vertex(string x, string y, string z)
         {
-            /*
-            coords[0] = new Q3_12(float.Parse(x));
-            coords[1] = new Q3_12(float.Parse(y));
-            coords[2] = new Q3_12(float.Parse(z));
-            */
-            coords[0] = (float.Parse(x));
-            coords[1] = (float.Parse(y));
-            coords[2] = (float.Parse(z));
-        }
-
-	public Vertex(float x, float y, float z)
-        {
-            coords[0] = x;
-            coords[1] = y;
-            coords[2] = z;
+            data[0] = (float.Parse(x));
+            data[1] = (float.Parse(y));
+            data[2] = (float.Parse(z));
         }
 
         public override string ToString()
